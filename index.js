98441127b7195ced471b13074fd1d468a7ed3c2f
screeeen <miguel@screenimplosion.com>
2019-04-24 15:02:48 +0200

    test done

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
-  average() {}
-  sum() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    return this.items.sort(function(a,b){return a-b});
+  }
+
+  get(pos) {
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length < 1) throw new Error("EmptySortedList");
+    return this.items[this.length-1];
+
+  }
+  min() {
+    if (this.length < 1) throw new Error("EmptySortedList");
+    return this.items[0];
+  }
+
+  sum() {
+    let arrSum = 0;
+    for (let i = 0; i < this.length; i++) {
+      arrSum += this.items[i];
+    }
+    return arrSum;
+
+  }
+
+  average() {
+    if (this.length < 1) throw new Error("EmptySortedList");
+    return this.sum()/this.length;
+  }
 };
 
 module.exports = SortedList;
+
